88f1d7a22f520f20e616eb1334761d844e822be1
VR1306 <vijay13.doodleblue@gmail.com>
2022-05-12 12:45:04 +0530

    Assignment2

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './World.css';
+
+ class Hello extends Component {
+  render() {
+    return (
+      <div>
+          <h2>Hello World</h2>
+      </div>
+    )
+  }
+}
+
+export default Hello;
